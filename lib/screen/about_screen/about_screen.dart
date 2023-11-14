bc1b258507c338ac48f56abe9b0295ac2814c674
arjun2020-py <arjunkoodathil@gmail.com>
2023-11-15 00:01:54 +0530

    about screen is done

--- lib/screen/about_screen/about_screen.dart
+++ lib/screen/about_screen/about_screen.dart
@@ -1,10 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get/get.dart';
+import 'package:portfolio/helper/custom_widgets/custom_text_widget.dart';
+import 'package:portfolio/interlization/interlization.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+            onPressed: () {
+              Get.back();
+            },
+            icon: Icon(
+              Icons.arrow_back_ios_new,
+              color: Colors.white,
+            )),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: CircleAvatar(
+                radius: 100,
+                backgroundImage: AssetImage(
+                  'images/assets/profile.png',
+                ),
+              ),
+            ),
+            CustomTextWidget(
+                fontSize: 20, text: 'Hello I am', color: Colors.white),
+            CustomTextWidget(
+                fontSize: 25, text: LocalName.name.tr, color: Colors.white),
+            CustomTextWidget(
+                fontSize: 18,
+                text: LocalName.flutterDeveloper.tr,
+                color: Colors.white),
+            ElevatedButton(onPressed: () {}, child: Text('Hire Me')),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(
+                      FontAwesomeIcons.instagram,
+                      color: Colors.white,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(
+                      FontAwesomeIcons.linkedin,
+                      color: Colors.white,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    icon: FaIcon(
+                      FontAwesomeIcons.github,
+                      color: Colors.white,
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
